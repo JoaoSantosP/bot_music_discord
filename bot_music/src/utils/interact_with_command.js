152d28f33a6dce8368cf9d87d97d3a8d3e6663f2
Joao vitor santos <jvitor.spaula@gmail.com>
2022-12-07 16:09:45 -0300

    feat: refactor file interact_with_command and create join_channel function

--- bot_music/src/utils/interact_with_command.js
+++ bot_music/src/utils/interact_with_command.js
@@ -1,5 +1,5 @@
-const { VoiceConnection, VoiceConnectionStatus, VoiceConnectionDisconnectReason, joinVoiceChannel, entersState } = require("@discordjs/voice");
-const { GuildMember } = require("discord.js");
+const { joinChannel } = require("./join_channel");
+
 
 module.exports = {
   interact_with_command: async (data) => {
@@ -18,48 +18,26 @@ module.exports = {
     if (cmd.length === 0) return;
     let command = client.commands.get(cmd);
     if (!command) command = client.commands.get(client.aliases.get(cmd))
-    if (command.name === "play") {
-      if (!message.guildId) return;
 
-      if (!subscription) {
-        if (message.member instanceof GuildMember && message.member.voice.channel) {
-          const channel = message.member.voice.channel;
-          // const joinChannel = 
-          subscription = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: channel.guild.id,
-            adapterCreator: channel.guild.voiceAdapterCreator,
-          });
-
-          subscription.on('stateChange', async (oldState, newState) => {
-            if (newState.status === VoiceConnectionStatus.Disconnected) {
-              if (newState.reason === VoiceConnectionDisconnectReason.WebSocketClose && newState.closeCode === 4014) {
-                try {
-                  await entersState(subscription, VoiceConnectionStatus.Connecting, 5_000)
-
-                } catch (error) {
-                  console.error(error)
-                  subscription.Destroy();
-                }
-              }
-            }
-          })
-          subscriptions.set(message.guildId, subscription);
-          setTimeout(() => {
-            subscription.destroy()
-            subscriptions.delete(message.guildId)
-          }, 5000)
-          // console.log(subscriptions)
-        }
-      }
-    }
-
-    /* 
-        try {
-          await command.play(client, message, args)
-    
-        } catch (error) {
-          console.error(error)
-        } */
+    const dataParams = { subscription, subscriptions, message, command }
+    joinChannel(dataParams)
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+/*
+    try {
+      await command.play(client, message, args)
+ 
+    } catch (error) {
+      console.error(error)
+    } */
